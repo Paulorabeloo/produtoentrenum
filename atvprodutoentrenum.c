67df9102a0f58d62874cd870725daff33ad27c3f
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-01-23 18:28:51 -0300

    produtoentrenum

---
+++ atvprodutoentrenum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int n1, n2, n3, n4, produto;
+    scanf("%i%i%i%i",&n1, &n2, &n3, &n4);
+
+    produto = n1 * n2 * n3 * n4;
+    printf("O produto desses quatro numeros é: %i", produto);
+return 0;
+}
